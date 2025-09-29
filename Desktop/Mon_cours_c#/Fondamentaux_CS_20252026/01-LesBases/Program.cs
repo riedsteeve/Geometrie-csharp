241ccfe25c629f72be3777e0336849a591c47f8b
riedsteeve <145045537+riedsteeve@users.noreply.github.com>
2025-09-29 14:20:24 +0200

    Steeve

---
+++ Desktop/Mon_cours_c#/Fondamentaux_CS_20252026/01-LesBases/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _01_LesBases
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Worlddddddddd!");
+        }
+    }
+}
